0695fd03f7cbd20e7e69e2eae6d52e8b1c58da1b
phisitsak <phisitsak.oum@gmail.com>
2020-02-20 23:18:46 +0700

    success auth

---
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -0,0 +1,28 @@
+import { AuthService } from './../_services/auth.service';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+model: any = {};
+@Output() cancelRegister = new EventEmitter();
+  constructor(private auth: AuthService) { }
+
+  ngOnInit() {
+  }
+  register() {
+    this.auth.register(this.model).subscribe(()=>{
+console.log('register success');
+    }, error => {
+       console.log(error);
+    });
+  }
+  cancel() {
+    this.cancelRegister.emit(false);
+  }
+
+}
