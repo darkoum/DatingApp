f5378e57b68740d5c34321ecce5460c72f15d743
phisitsak <phisitsak.oum@gmail.com>
2020-02-22 12:06:42 +0700

    router

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,9 +1,12 @@
 
 
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './routes';
 
 import { HttpClientModule } from '@angular/common/http';
 import { NavComponent } from './nav/nav.component';
@@ -13,19 +16,26 @@ import { ErrorInterceptorProvider } from './_services/error.interseptor';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 // or
 import { BsDropdownModule } from 'ngx-bootstrap';
+import { MemberListComponent } from './member-list/member-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
 @NgModule({
    declarations: [
       AppComponent,
       NavComponent,
       HomeComponent,
-      RegisterComponent
+      RegisterComponent,
+      MemberListComponent,
+      ListsComponent,
+      MessagesComponent
    ],
    imports: [
       BrowserModule,
       HttpClientModule,
       FormsModule,
       BrowserAnimationsModule,
-      BsDropdownModule.forRoot()
+      BsDropdownModule.forRoot(),
+      RouterModule.forRoot(appRoutes)
    ],
    providers: [
       ErrorInterceptorProvider
--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { AlertifyService } from './../_services/alertify.service';
 import { AuthService } from './../_services/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -9,7 +10,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
  model: any = {};
-  constructor(public authService: AuthService, private alertify: AlertifyService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService
+    , private router: Router) { }
 
   ngOnInit() {
   }
@@ -18,6 +20,8 @@ login() {
 this.alertify.success('logged in success');
   }, error => {
 this.alertify.error(error);
+  },() => {
+this.router.navigate(['/members']);
   });
 }
 loggedIn() {
@@ -26,5 +30,6 @@ loggedIn() {
 logout() {
  localStorage.removeItem('token');
  this.alertify.message('logged out');
+ this.router.navigate(['/home']);
 }
 }
